b78b53864202e4eb370383e626162608edfe8547
Wendy Machado <wendymachado.-@hotmail.com>
2026-05-21 16:05:56 -0300

    fix body parser

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const OpenAI = require('openai');
 const https = require('https');
 
 const app = express();
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
+
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
